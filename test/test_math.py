eb74286c2ff6082b4842af5e40bb91a6022c9fc2
Ferencsik Róbert <robert.ferencsik@gmail.com>
2026-03-22 21:05:27 +0100

    feat: Adding a example testing case for oop style

---
+++ test/test_math.py
@@ -0,0 +1,28 @@
+import pytest
+from math_own import Math
+
+class TestMath:
+    @pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (0, 0, 0),
+    (-1, -1, -2),
+    (100, 200, 300)
+    ])
+    def test_add_2_nums(self,a, b, expected):
+        name = Math(a, b)
+        assert name.add_2_nums(a, b) == expected
+        
+
+
+'''
+    @pytest.mark.parametrize("a, b", [
+        ("1", 2),
+        (None, 2),      
+        ([1], 2),      
+        (1, {"b": 2}),  
+    ])
+    def test_add_2_nums_invalid(self, a, b):
+        obj = Math(a, b)
+        with pytest.raises(TypeError):
+            obj.add_2_nums(a, b)
+'''
