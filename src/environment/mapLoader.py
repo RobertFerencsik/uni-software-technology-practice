befe8c2fafb3d400d1a88504c6fbe19c3f69f68e
GyongyosiDora <gyongyosidora18@gmail.com>
2026-03-26 21:45:10 +0100

    added map layouts and random loader

---
+++ src/environment/mapLoader.py
@@ -0,0 +1,17 @@
+import random
+
+from environment.mapLayouts import map1, map2, map3
+
+class MapLoader:
+    def __init__(self):
+        self.maps = [
+            map1.generate,
+            map2.generate,
+            map3.generate
+        ]
+
+    # random map választás
+    def load_random_map(self):
+        chosen = random.choice(self.maps)
+
+        return chosen
