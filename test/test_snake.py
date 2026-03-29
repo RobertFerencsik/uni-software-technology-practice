555b4bf182f5661876c9948d291c0713ae53ab8c
Robert Ferencsik <robert.ferencsik@gmail.com>
2026-03-29 21:15:23 +0200

    test: Eliminate initialization using fixture and removing class for cleaner implementation

--- test/test_snake.py
+++ test/test_snake.py
@@ -1,30 +1,28 @@
 import pytest
 from snake import Snake
 
-class TestSnake:
-    def test_init(self):
-        snake = Snake()
-        assert list(snake.body) == [(5, 5)]
-        assert snake.direction == (1, 0)
-        assert not snake.grow_pending
+@pytest.fixture
+def snake():
+    return Snake()
 
-    def test_move(self):
-        snake = Snake()
-        snake.move()
-        assert list(snake.body) == [(6, 5)]
+def test_init(snake):
+    assert list(snake.body) == [(5, 5)]
+    assert snake.direction == (1, 0)
+    assert not snake.grow_pending
 
-    def test_grow(self):
-        snake = Snake()
-        snake.grow()
-        snake.move()
-        assert list(snake.body) == [(5, 5), (6, 5)]
+def test_move(snake):
+    snake.move()
+    assert list(snake.body) == [(6, 5)]
 
-    def test_check_self_collision(self):
-        snake = Snake()
-        snake.body = [(5, 5), (6, 5), (6, 6), (5, 5)]
-        assert snake.check_self_collision() is True
+def test_grow(snake):
+    snake.grow()
+    snake.move()
+    assert list(snake.body) == [(5, 5), (6, 5)]
 
-    def test_change_direction(self):
-        snake = Snake()
-        snake.change_direction((0, 1))
-        assert snake.direction == (0, 1)
+def test_check_self_collision(snake):
+    snake.body = [(5, 5), (6, 5), (6, 6), (5, 5)]
+    assert snake.check_self_collision() is True
+
+def test_change_direction(snake):
+    snake.change_direction((0, 1))
+    assert snake.direction == (0, 1)
