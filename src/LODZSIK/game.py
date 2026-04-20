1694364bd567ed0b3d82c6bd5e85f090a6b6dae7
Robert Ferencsik <robert.ferencsik@gmail.com>
2026-04-20 11:20:42 +0200

    fix: change maploader module name to use snake case according to pep 8

--- src/LODZSIK/game.py
+++ src/LODZSIK/game.py
@@ -3,7 +3,7 @@ from point import Point
 
 from environment.board import Board
 from environment.food import Food
-from environment.maploader import MapLoader
+from environment.map_loader import MapLoader
 
 
 class Game:
